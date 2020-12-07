36a6b08cdb1c546ac86f5d64b749fd77e063c1bf
kalebtolentino <71675572+kalebtolentino@users.noreply.github.com>
2020-12-07 15:27:38 -0800

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,26 @@
+class Polygon {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, 30, options);
+      this.diameter = 20;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      fill("red");
+      circle(0, 0, this.diameter);
+      pop();
+    }
+  };
+  
